23002cfc44fd2c69708cda168c8d5d05d22a545d
gameenudara <gameenudara567@gmail.com>
2024-03-17 14:21:36 +0530

    adding dustbin using post mapping

--- src/main/java/JUN_WE_29/Garbage_Collection_Api/controller/DustbinController.java
+++ src/main/java/JUN_WE_29/Garbage_Collection_Api/controller/DustbinController.java
@@ -1,4 +1,12 @@
 package JUN_WE_29.Garbage_Collection_Api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class DustbinController {
+
+    @PostMapping("/dustbins")
+    public void addDusbin(){
+        System.out.println("add dustbit");
+    }
 }
