cd6401619e5b06fcd46514da63c11f494061c2dd
gameenudara <gameenudara567@gmail.com>
2024-03-19 23:53:49 +0530

    Create model

--- src/main/java/JUN_WE_29/Garbage_Collection_Api/model/Dustbin.java
+++ src/main/java/JUN_WE_29/Garbage_Collection_Api/model/Dustbin.java
@@ -1,4 +1,32 @@
 package JUN_WE_29.Garbage_Collection_Api.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "dustbin")
+@Data
 public class Dustbin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private float quantity;
+
+    private float smell;
+
+    private String location;
+
+    @ManyToMany
+    @JoinTable(
+            name = "dustbin_user",
+            joinColumns = @JoinColumn(name = "dustbin_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> userList;
 }
--- src/main/java/JUN_WE_29/Garbage_Collection_Api/model/User.java
+++ src/main/java/JUN_WE_29/Garbage_Collection_Api/model/User.java
@@ -1,4 +1,27 @@
 package JUN_WE_29.Garbage_Collection_Api.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String phone;
+
+    @ManyToMany(mappedBy = "userList")
+    private List<Dustbin> dustbinList;
 }
